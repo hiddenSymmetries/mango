b5171444248b5ff9667f9120b78871af1f53d2aa
Matt Landreman <mattland@umd.edu>
2019-10-11 10:34:58 -0400

    Added nondifferentiable and quadratic examples

---
+++ examples/src/nondifferentiable_c.cpp
@@ -0,0 +1,67 @@
+#define N_dims 3
+
+#include<iostream>
+#include<cstring>
+#include<mpi.h>
+#include<math.h>
+#include "mango.hpp"
+
+void objective_function(int*, const double*, double*, int*);
+
+int main(int argc, char *argv[]) {
+  int ierr;
+
+  std::cout << "Hello world from nondifferentiable_c.\n";
+
+  ierr = MPI_Init(&argc, &argv);
+  if (ierr != 0) {
+    std::cout << "\nError in MPI_Init.\n";
+    exit(1);
+  }
+
+  /*  int N_procs, mpi_rank;
+  MPI_Comm_size(MPI_COMM_WORLD, &N_procs);
+  MPI_Comm_rank(MPI_COMM_WORLD, &mpi_rank);
+  std::cout << "N_procs=" << N_procs << "mpi_rank=" << mpi_rank;
+
+  std::cout << MANGO_PETSC_NM << "\nHello world!\n";
+  */
+
+  double state_vector[N_dims];
+  memset(state_vector, 0, N_dims*sizeof(double));
+
+  mango::problem myprob(N_dims, state_vector, &objective_function, argc, argv);
+
+  //std::cout << "Here comes state vector:" << *(myprob.state_vector);
+  /*
+  myprob.state_vector[0] =  5.0;
+  myprob.state_vector[1] = 10.0;
+  myprob.state_vector[2] = 15.0;
+  myprob.state_vector[3] = 20.0;
+  */
+
+  /*  myprob.set_algorithm(mango::PETSC_POUNDERS); */
+  //  myprob.set_algorithm(mango::NLOPT_LD_LBFGS);
+  // myprob.set_algorithm("nlopt_ln_neldermead");
+  myprob.read_input_file("../input/mango_in.nondifferentiable_c");
+  myprob.output_filename = "../output/mango_out.nondifferentiable_c";
+  myprob.mpi_init(MPI_COMM_WORLD);
+  /* myprob.centered_differences = true; */
+
+  myprob.optimize();
+
+  MPI_Finalize();
+
+  return 0;
+}
+
+
+void objective_function(int* N, const double* x, double* f, int* failed) {
+  int j;
+  std::cout << "C objective function called with N="<< *N << "\n";
+  *f = 0;
+  for (int j=1; j <= *N; j++) {
+    *f += fabs(x[j-1] - j) / j;
+  }
+  *failed = false;
+}
---
+++ examples/src/quadratic_f.F90
@@ -0,0 +1,89 @@
+#define N_dim 3
+
+program quadratic
+
+  use mango
+  !use iso_c_binding
+
+  implicit none
+
+  include 'mpif.h'
+  
+  integer :: ierr, N_procs, mpi_rank, data(1)
+  logical :: proc0
+  !type(mango_least_squares_problem) :: problem
+  type(mango_problem) :: problem
+  double precision, dimension(N_dim) :: state_vector
+  !external objective_function
+  !procedure(objective_function_interface), pointer :: objective_function
+  integer :: dummy = 13
+
+  !---------------------------------------------
+
+  print *,"Hello world from quadratic_f"
+  !print *,"c_funloc(objective_function):",c_funloc(objective_function)
+  call mpi_init(ierr)
+
+  state_vector = 0
+  call mango_problem_create(problem,N_dim,state_vector,dummy,objective_function)
+  print *,"Here comes state vector:",state_vector
+  !call mango_set_algorithm(problem, 2)
+  !call mango_set_algorithm_from_string(problem, "nlopt_ln_praxis")
+  call mango_read_input_file(problem, "../input/mango_in.quadratic_f")
+  call mango_set_output_filename(problem, "../output/mango_out.quadratic_f")
+  call mango_mpi_init(problem, MPI_COMM_WORLD)
+
+!  if (mango_proc0_worker_groups(problem)) then
+     call mango_optimize(problem)
+
+!     ! Make workers stop
+!     data = -1
+!     call mpi_bcast(data,1,MPI_INTEGER,0,mango_mpi_comm_worker_groups(problem),ierr)
+!     if (ierr .ne. 0) print *,"Error A on proc0!"
+!  else
+!     call worker(problem)
+!  end if
+
+  call mango_problem_destroy(problem)
+
+  call mpi_finalize(ierr)
+
+  print *,"Good bye!"
+
+contains
+
+!subroutine objective_function(N, x, f)
+!  implicit none
+!  integer, intent(in) :: N
+!  double precision, intent(in) :: x(N)
+!  double precision, intent(out) :: f
+!
+!  print *,"Hi from fortran. N=",N," size(x)=",size(x)
+!  f = sum((x-2)*(x-2))
+!  print *,"In fortran, x=",x,", f=",f
+!
+!end subroutine objective_function
+
+subroutine objective_function(N, x, f, failed)
+  use iso_c_binding
+  implicit none
+  integer(C_int), intent(in) :: N
+  real(C_double), intent(in) :: x(N)
+  real(C_double), intent(out) :: f
+  integer(C_int), intent(out) :: failed
+  integer :: j
+
+  print *,"Hi from fortran. N=",N," size(x)=",size(x)
+  !f = sum((x-2)*(x-2))
+  !print *,"In fortran, x=",x,", f=",f
+
+  f = 0
+  do j = 1, N
+     f = f + (x(j) - j) * (x(j) - j) / (j*j)
+  end do
+
+  failed = 0
+
+end subroutine objective_function
+
+end program quadratic
