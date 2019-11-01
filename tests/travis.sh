a989c7b765ce50c6faf3e25d7e3d44fa6d340610
Matt Landreman <mattland@umd.edu>
2019-11-01 16:50:26 -0400

    Listing apt packages in travis

--- tests/travis.sh
+++ tests/travis.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+set -ex
+# In the above line, "set -x" causes this script to exit as soon as any line fails. "set -e" causes each line of this script to be printed (with a + in front) before it is executed, so if a step fails, you can see from the travis log what command failed.
+
 echo Hello from travis.sh
 
 pwd
