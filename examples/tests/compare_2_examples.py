32e2151b6e1bb783005ec438c316f5add7c327e6
Matt Landreman <mattland@umd.edu>
2019-11-23 09:52:13 -0500

    Modified verify_language_independence tests to only specify algorithms to exclude

--- examples/tests/compare_2_examples.py
+++ examples/tests/compare_2_examples.py
@@ -11,9 +11,10 @@ def test_float_equality(x1,x2,tol):
         return True
     return np.abs(x2-x1) / avg < tol
 
-def compare_2_examples(filename1, filename2, algorithms):
+def compare_2_examples(filename1, filename2, algorithms_to_exclude):
     solution_vector_tolerance = 1.0e-17
     objective_function_tolerance = 1.0e-17
+    verbose = False
 
     # Read output from example 1:
     big_filename1 = 'output/short_summary_mpi.'+filename1
@@ -49,82 +50,71 @@ def compare_2_examples(filename1, filename2, algorithms):
         print('Error! Unable to convert line 4 of '+big_filename1+' to an integer.')
         raise
 
+    if len(lines1) != len(lines2):
+        print('Error! Number of lines in '+big_filename1+' does not equal number of lines in '+big_filename2)
+        exit(1)
+
     # Ignore header lines:
-    lines1 = lines1[5:]
-    lines2 = lines2[5:]
+    for j_line in range(5,len(lines1)):
+        results1 = lines1[j_line].split(',')
+        results2 = lines2[j_line].split(',')
 
-    for j_algorithm in range(len(algorithms)):
-        # Extract the lines from the output file associated with a given algorithm
-        algorithm = algorithms[j_algorithm]
-        results1 = []
-        results2 = []
-        for line in lines1:
-            splitline = line.split(',')
-            if splitline[0] == algorithm:
-                results1.append(splitline)
-        for line in lines2:
-            splitline = line.split(',')
-            if splitline[0] == algorithm:
-                results2.append(splitline)
-        if len(results1) == 0:
-            print('Error! No results for algorithm '+algorithm+' in file '+big_filename1)
-            exit(1)
-        if len(results2) == 0:
-            print('Error! No results for algorithm '+algorithm+' in file '+big_filename2)
-            exit(1)
-        if len(results1) != len(results2):
-            print('Error! For algorithm '+algorithm+', the number of MPI options in '+big_filename1+' does not equal the corresponding number in '+big_filename2)
+        # Algorithm, N_procs, and N_worker_groups should always match:
+        if (results1[0] != results2[0]) or (results1[1] != results2[1]) or (results1[2] != results2[2]):
+            print('Error! Lines '+str(j_line)+' of '+big_filename1+' and '+big_filename2+' to not match all of the first 3 elements.')
+            print(big_filename1+': ',results1)
+            print(big_filename2+': ',results2)
             exit(1)
 
-        for j_mpi in range(len(results1)):
-            # Make sure the same value of N_procs was used:
-            index = 1
-            N_procs1 = int(results1[j_mpi][index])
-            N_procs2 = int(results2[j_mpi][index])
-            if N_procs1 != N_procs2:
-                print('Error! For algorithm '+algorithm+' line '+str(j_mpi)+', N_procs does not match between '+big_filename1+' and '+big_filename2)
-                exit(1)
-
-            # Make sure the same value of N_worker_groups was used:
-            index = 2
-            N_worker_groups1 = int(results1[j_mpi][index])
-            N_worker_groups2 = int(results2[j_mpi][index])
-            if N_worker_groups1 != N_worker_groups2:
-                print('Error! For algorithm '+algorithm+' line '+str(j_mpi)+', N_worker_groups does not match between '+big_filename1+' and '+big_filename2)
-                exit(1)
-
-            # If both examples failed for these parameters, that is fine, and we can consider it a match.
-            index = 3
-            if results1[j_mpi][index].strip() == 'FAILED' and results2[j_mpi][index].strip() == 'FAILED':
+        # See if this is an algorithm to skip:
+        algorithm1 = results1[0].strip()
+        exclude = False
+        for algorithm in algorithms_to_exclude:
+            if algorithm == algorithm1:
+                exclude = True
                 continue
+        if exclude:
+            if verbose:
+                print('Skipping excluded algorithm:',results1)
+            continue
+        else:
+            if verbose:
+                print('Processing line:',results1)
 
-            # Make sure the number of function evaluations was the same:
-            index = 3
-            function_evaluations1 = int(results1[j_mpi][index])
-            function_evaluations2 = int(results2[j_mpi][index])
-            if function_evaluations1 != function_evaluations2:
-                print('Error! For algorithm '+algorithm+' line '+str(j_mpi)+', function_evaluations does not match between '+big_filename1+' and '+big_filename2)
-                print('function_evaluations1 = ',results1[j_mpi][index],',  function_evaluations2 = ',results2[j_mpi][index])
+        # If both examples failed for these parameters, that is fine, and we can consider it a match.
+        index = 3
+        if results1[index].strip() == 'FAILED' and results2[index].strip() == 'FAILED':
+            if verbose:
+                print('2 failures count as a match')
+            continue
+
+        # Make sure the number of function evaluations was the same:
+        index = 3
+        function_evaluations1 = int(results1[index])
+        function_evaluations2 = int(results2[index])
+        if function_evaluations1 != function_evaluations2:
+            print('Error! On line '+str(j_line)+', function_evaluations does not match between '+big_filename1+' and '+big_filename2)
+            print('function_evaluations1 = ',results1[index],',  function_evaluations2 = ',results2[index])
+            exit(1)
+
+        # Make sure the solution vector was the same:
+        for j_parameter in range(N_parameters):
+            index = 4+j_parameter
+            x1 = float(results1[index])
+            x2 = float(results2[index])
+            if not test_float_equality(x1,x2,solution_vector_tolerance):
+                print('Error! On line '+str(j_line)+', x['+str(j_parameter)+'] does not match between '+big_filename1+' and '+big_filename2)
+                print('x1 = ',results1[index].strip(),',  x2 = ',results2[index].strip())
                 exit(1)
 
-            # Make sure the solution vector was the same:
-            for j_parameter in range(N_parameters):
-                index = 4+j_parameter
-                x1 = float(results1[j_mpi][index])
-                x2 = float(results2[j_mpi][index])
-                if not test_float_equality(x1,x2,solution_vector_tolerance):
-                    print('Error! For algorithm '+algorithm+' line '+str(j_mpi)+', x['+str(j_parameter)+'] does not match between '+big_filename1+' and '+big_filename2)
-                    print('x1 = ',results1[j_mpi][index].strip(),',  x2 = ',results2[j_mpi][index].strip())
-                    exit(1)
-
-            # Make sure the final objective function was the same:
-            index = 4+N_parameters
-            f1 = float(results1[j_mpi][index])
-            f2 = float(results2[j_mpi][index])
-            if not test_float_equality(f1,f2,objective_function_tolerance):
-                print('Error! For algorithm '+algorithm+' line '+str(j_mpi)+', the objective function does not match between '+big_filename1+' and '+big_filename2)
-                print('f1 = ',results1[j_mpi][index].strip(),',  f2 = ',results2[j_mpi][index].strip())
-                exit(1)
+        # Make sure the final objective function was the same:
+        index = 4+N_parameters
+        f1 = float(results1[index])
+        f2 = float(results2[index])
+        if not test_float_equality(f1,f2,objective_function_tolerance):
+            print('Error! On line '+str(j_line)+', the objective function does not match between '+big_filename1+' and '+big_filename2)
+            print('f1 = ',results1[index].strip(),',  f2 = ',results2[index].strip())
+            exit(1)
 
 
     print('All results compared in '+big_filename1+' and '+big_filename2+' are consistent.')
--- examples/tests/verify_language_independence.py
+++ examples/tests/verify_language_independence.py
@@ -11,39 +11,11 @@ except:
 
 def verify_language_independence():
 
-    base_algorithms = [\
-            'petsc_nm',\
-            'nlopt_ln_bobyqa',\
-            'nlopt_ln_neldermead',\
-            'nlopt_ld_mma',\
-            'nlopt_ld_ccsaq',\
-            'nlopt_ld_slsqp',\
-            'nlopt_ld_lbfgs',\
-            'nlopt_ld_tnewton_precond_restart',\
-            'nlopt_ld_tnewton_precond',\
-            'nlopt_ld_tnewton_restart',\
-            'nlopt_ld_tnewton',\
-            'nlopt_ld_var1',\
-            'nlopt_ln_sbplx',
-            'nlopt_ld_var2']
+    nondeterministic_algorithms = ['petsc_pounders','nlopt_gn_direct_l_rand','nlopt_gn_direct_l_rand_noscal','nlopt_gn_crs2_lm','nlopt_ln_praxis']
 
-    # Do not include the 'rand' algorithms, praxis, nlopt_gn_crs2_lm, or petsc_pounders, since they are not deterministic
-    extra_algorithms = [\
-        'nlopt_gn_direct',\
-            'nlopt_gn_direct_l',\
-            'nlopt_gn_direct_noscal',\
-            'nlopt_gn_direct_l_noscal',\
-            'nlopt_gn_orig_direct',\
-            'nlopt_gn_orig_direct_l']
-
-    least_squares_algorithms = base_algorithms
-
-    compare_2_examples('quadratic_c','quadratic_f',least_squares_algorithms + extra_algorithms)
-    compare_2_examples('rosenbrock_c','rosenbrock_f',least_squares_algorithms)
-
-    algorithms = base_algorithms + ['blorp','foo','glurb']
-
-    compare_2_examples('nondifferentiable_c','nondifferentiable_f',algorithms)
+    compare_2_examples('quadratic_c','quadratic_f',nondeterministic_algorithms)
+    compare_2_examples('rosenbrock_c','rosenbrock_f',nondeterministic_algorithms)
+    compare_2_examples('nondifferentiable_c','nondifferentiable_f',nondeterministic_algorithms)
 
 # Allow this module to be run as a standalone script.
 if __name__ == '__main__':
