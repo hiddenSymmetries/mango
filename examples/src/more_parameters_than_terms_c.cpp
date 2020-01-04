2be93527d4be12ff515dc68fcc05ecd9e74bd66a
Matt Landreman <mattland@umd.edu>
2020-01-04 15:53:55 -0500

    Added more_parameters_than_terms example

---
+++ examples/src/more_parameters_than_terms_c.cpp
@@ -0,0 +1,131 @@
+// f = [z - x^2 - y^2)]^2 + [(x + y + z - (-1)]^2
+// The first term is minimized for points on the graph z = x^2 + y^2.
+// The second term is minimized for points on the plane x + y + z = -1.
+// Since this graph and plane do not intersect, the optimum of the total f is >0.
+// The optimum is achieved at a point near the point of closest approach between the graph and plane.
+// State vector at optimum: [-0.5, -0.5, 0.25]
+// Minimum objective function: f = 0.125
+// Residual functions at optimum: [-0.25, -0.75]
+
+#define verbose_level 0
+
+#include<iostream>
+#include<iomanip>
+#include<mpi.h>
+#include<stdlib.h>
+#include<cassert>
+#include "mango.hpp"
+
+#define N_parameters 3
+#define N_terms 2
+
+void residual_function(int*, const double*, int*, double*, int*, mango::problem*, void*);
+
+void worker(mango::problem*);
+
+int main(int argc, char *argv[]) {
+  int ierr;
+
+  if (verbose_level > 0) std::cout << "Hello world from more_parameters_than_terms_c." << std::endl;
+
+  ierr = MPI_Init(&argc, &argv);
+  if (ierr != 0) {
+    std::cerr << "Error in MPI_Init." << std::endl;
+    exit(1);
+  }
+
+  double state_vector[N_parameters] = {0.0, 0.0, 0.0};
+  double sigmas[N_terms] = {1.0, 1.0};
+  double targets[N_terms] = {0.0, -1.0};
+
+  if (verbose_level > 0) {
+    std::cout << "Is foobar a valid algorithm? " << mango::does_algorithm_exist("foobar") << "\n";
+    std::cout << "Is petsc_nm a valid algorithm? " << mango::does_algorithm_exist("petsc_nm") << "\n";
+  }
+
+  double best_residual_function[N_terms];
+  mango::problem myprob(N_parameters, state_vector, N_terms, targets, sigmas, best_residual_function, &residual_function, argc, argv);
+
+  myprob.verbose = verbose_level;
+  myprob.read_input_file("../input/mango_in.more_parameters_than_terms_c");
+  myprob.output_filename = "../output/mango_out.more_parameters_than_terms_c";
+  myprob.mpi_init(MPI_COMM_WORLD);
+  myprob.centered_differences = true; 
+  myprob.max_function_evaluations = 2000;
+
+  // Pass some data to the objective function
+  int data = 7;
+  myprob.user_data = &data;
+
+  double lower_bounds[N_parameters];
+  double upper_bounds[N_parameters];
+  for (int j=0; j<N_parameters; j++) {
+    lower_bounds[j] = -5.0;
+    upper_bounds[j] =  5.0;
+  }
+
+  myprob.set_bound_constraints(lower_bounds, upper_bounds);
+
+  double best_objective_function;
+  if (myprob.mpi_partition.get_proc0_worker_groups()) {
+    best_objective_function = myprob.optimize();
+    myprob.mpi_partition.stop_workers();
+  } else {
+    worker(&myprob);
+  }
+
+  if (myprob.mpi_partition.get_proc0_world() && (verbose_level > 0)) {
+    std::cout << "Best state vector: " << std::setprecision(16);
+    for (int j=0; j<N_parameters; j++) std::cout << state_vector[j] << "  ";
+    std::cout << "\nBest objective function: " << best_objective_function << "\nBest residual vector:";
+    for (int j=0; j<2; j++) std::cout << best_residual_function[j] << "  ";
+    std::cout << "\nBest function evaluation was " << myprob.get_best_function_evaluation() << "\n";
+  }
+
+  MPI_Finalize();
+
+  return 0;
+}
+
+
+void residual_function(int* N, const double* x, int* M, double* f, int* failed, mango::problem* this_problem, void* void_user_data) {
+  int j;
+  if (verbose_level > 0) std::cout << "C residual function called on proc " << this_problem->mpi_partition.get_rank_world() << " with N="<< *N << ", M=" << *M << "\n";
+
+  // Verify that the user data was passed successfully.
+  int* user_data = (int*)void_user_data;
+  assert(*user_data == 7);
+
+  assert(*N == N_parameters);
+  assert(*M == N_terms);
+
+  // Mobilize the workers in the group with this group leader:
+  this_problem->mpi_partition.mobilize_workers();
+
+  f[0] = x[2] - x[0] * x[0] - x[1] * x[1];
+  f[1] = x[0] + x[1] + x[2];
+
+  *failed = false;
+  
+  if (verbose_level > 0) {
+    std::cout << "state_vector:";
+    for (j=0; j < *N; j++) {
+      std::cout << std::setw(24) << std::setprecision(15) << x[j];
+    }
+    std::cout << "\n";
+    std::cout << "residual:";
+    for (j=0; j < *M; j++) {
+      std::cout << std::setw(24) << std::setprecision(15) << f[j];
+    }
+    std::cout << "\n" << std::flush;
+  }
+}
+
+
+void worker(mango::problem* myprob) {
+  while (myprob->mpi_partition.continue_worker_loop()) {
+    // For this problem, the workers don't actually do any work.
+    if (verbose_level > 0) std::cout << "Proc " << std::setw(5) << myprob->mpi_partition.get_rank_world() << " could do some work here." << std::endl;
+  }
+  if (verbose_level > 0) std::cout << "Proc " << std::setw(5) << myprob->mpi_partition.get_rank_world() << " is exiting." << std::endl;
+}
