77a1c070c7a70ef874b5421e908d67c7a3741d70
Matt Landreman <mattland@umd.edu>
2020-03-13 12:17:44 -0400

    Levenberg-Marquardt with parallel line search appears to be working

--- src/algorithms/levenberg_marquardt.hpp
+++ src/algorithms/levenberg_marquardt.hpp
@@ -1,4 +1,5 @@
 namespace mango::levenberg_marquardt {
-  int fun1();
+  double compute_lambda_increase_factor(const int);
+  void compute_lambda_grid(const int, const double, double*);
 }
 
--- src/algorithms/tests/levenberg_marquardt_tests.cpp
+++ src/algorithms/tests/levenberg_marquardt_tests.cpp
@@ -1,6 +1,14 @@
 #include "catch.hpp"
 #include "levenberg_marquardt.hpp"
 
+
 TEST_CASE("levenberg_marquardt","[algorithm][levenberg_marquardt]") {
-  CHECK(mango::levenberg_marquardt::fun1() == 17);
+  // If N_line_search==1, the normalized lambda grid should be [1.0] for any lambda_step
+  int N_line_search = 1;
+  auto log_step = GENERATE(range(0,5)); // so log_step = 0, 1, 2, 3, or 4.
+  double lambda_step = exp(log_step - 2.0);
+  double lambda_grid;
+  mango::levenberg_marquardt::compute_lambda_grid(N_line_search, lambda_step, &lambda_grid);
+  CHECK(lambda_grid == Approx(1.0).epsilon(1e-13));
 }
+
