654eff4c54c376d872aa82f8ca024d5b67935e8a
Matt Landreman <mlan@draco04.mpcdf.mpg.de>
2020-03-16 16:55:39 +0100

    Tweaks for draco

--- src/algorithms/levenberg_marquardt.hpp
+++ src/algorithms/levenberg_marquardt.hpp
@@ -1,5 +1,7 @@
-namespace mango::levenberg_marquardt {
-  double compute_lambda_increase_factor(const int);
-  void compute_lambda_grid(const int, const double, double*);
+namespace mango {
+  namespace levenberg_marquardt {
+    double compute_lambda_increase_factor(const int);
+    void compute_lambda_grid(const int, const double, double*);
+  }
 }
 
